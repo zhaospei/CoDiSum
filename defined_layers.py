79034d16913d32c509da656efe23fcb0ee619d53
zhaospei <dungbuit1k28@gmail.com>
2023-09-02 10:37:21 +0700

    Revert first commit

--- defined_layers.py
+++ defined_layers.py
@@ -1,6 +1,5 @@
 from keras import backend as K
-from keras.engine.base_layer import Layer
-from keras.engine.input_spec import InputSpec
+from keras.engine.topology import Layer, InputSpec
 from keras.layers import Input, GlobalAveragePooling1D, GlobalMaxPooling1D, Embedding, Lambda, GRU, Dense
 from keras.layers import Conv2D, AveragePooling2D, Bidirectional
 from keras.models import Model
@@ -376,7 +375,7 @@ def validate():
     x[0, 0, 0, :] = 1.
     # print(x)
     a = model.predict(x)
-    print(a)
+    print("OK", a)
 
 
 if __name__ == "__main__":
